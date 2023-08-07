7b1f888891c425c87e8ea2a6c96d0d4e6ef3baf3
Skand Purohit <skandpurohit@gmail.com>
2023-08-06 21:38:38 -0600

    Update kustomization.tf

--- terraform/clustergroups/cg_1/kustomization.tf
+++ terraform/clustergroups/cg_1/kustomization.tf
@@ -1,14 +1,12 @@
 # Create Tanzu Mission Control kustomization with attached set as default value.
-resource "tanzu-mission-control_kustomization" "create_cluster_kustomization" {
+resource "tanzu-mission-control_kustomization" "create_cluster_group_kustomization" {
   name = "tf-kustomization" # Required
 
   namespace_name = "tanzu-continuousdelivery-resources" #Required
 
   scope {
-    cluster {
-      name            = "eks.sp-eks-new.us-east-2.sp-eks-east-2-tf" # Required
-      provisioner_name        = "eks"    # Default: attached
-      management_cluster_name = "eks"    # Default: attached
+    cluster_group {
+      name = "tmc-multitenant-cluster-group" # Required
     }
   }
 
@@ -17,7 +15,7 @@ resource "tanzu-mission-control_kustomization" "create_cluster_kustomization" {
     labels      = { "key" : "value" }
   }
 
-  spec {
+    spec {
     path = "/" # Required
     prune = true
     interval = "5m" # Default: 5m
@@ -28,4 +26,6 @@ resource "tanzu-mission-control_kustomization" "create_cluster_kustomization" {
   }
 
   depends_on = [tanzu-mission-control_ekscluster.tf_eks_cluster, tanzu-mission-control_namespace.create_namespace, tanzu-mission-control_git_repository.create_cluster_git_repository]
+
+
 }
