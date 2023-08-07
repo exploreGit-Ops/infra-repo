65f0cd3b92b5927b21dd64b3e9d0f38df9d33217
Will Arroyo <warroyo7199008@gmail.com>
2023-08-07 14:18:49 -0600

    paths

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 module "cluster_group_gitops" {
-  source = "./clustergroups/cg_1/main.tf"
+  source = "clustergroups/cg_1/main.tf"
 }
 
 module "cluster_create" {
-  source = "./clustergroups/cg_1/cluster_1/main.tf"
+  source = "clustergroups/cg_1/cluster_1/main.tf"
   depends_on = [ module.cluster_group_gitops ]
 }
