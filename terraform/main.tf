5ca20965677b202ff9c3394079d69ad3cd79d0a4
Will Arroyo <warroyo7199008@gmail.com>
2023-08-07 15:17:10 -0600

    test secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+
+data "azurerm_key_vault" "explore-gitops" {
+  name                = "explore-gitops"
+  resource_group_name = "keyvaults"
+}
+data "azurerm_key_vault_secret" "tmc-endpoint" {
+  name         = "tmc-endpoint"
+  key_vault_id = data.azurerm_key_vault.explore-gitops.id
+}
+
+data "azurerm_key_vault_secret" "tmc-api-key" {
+  name         = "tmc-api-key"
+  key_vault_id = data.azurerm_key_vault.epxlore-gitops.id
+}
+
 module "cluster_group_gitops" {
   source = "./clustergroups/dev/"
 }
