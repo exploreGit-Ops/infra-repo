3bfd4adf7ed3daa14b233a17a6756bc154c9318f
Will Arroyo <warroyo7199008@gmail.com>
2023-08-16 13:01:53 -0600

    fix deprecation

--- terraform/modules/cluster_bootstrap/main.tf
+++ terraform/modules/cluster_bootstrap/main.tf
@@ -1,12 +1,12 @@
 
 
-resource "local_file" "kubeconfig" {
-    sensitive_content     = base64decode(var.kubeconfig)
+resource "local_sensitive_file" "kubeconfig" {
+    content     = base64decode(var.kubeconfig)
     filename = "${path.module}/kubeconfig"
 }
 
-resource "local_file" "akv-secret" {
-    sensitive_content     = templatefile("${path.module}/akv-secret.tftpl",{
+resource "local_sensitive_file" "akv-secret" {
+       content     = templatefile("${path.module}/akv-secret.tftpl",{
         azure_client_id = base64encode(var.azure_client_id)
         azure_client_secret = base64encode(var.azure_client_secret)
     })
@@ -29,6 +29,6 @@ resource "shell_script" "akv_bootstrap" {
            kubectl delete -f ${path.module}/akv-secret.yml
         EOF
   }
-  depends_on = [ local_file.kubeconfig,local_file.akv-secret ]
+  depends_on = [ local_sensitive_file.kubeconfig,local_sensitive_file.akv-secret ]
 
 }
